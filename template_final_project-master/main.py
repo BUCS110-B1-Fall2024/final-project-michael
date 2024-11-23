8860debefadd9cf4b3f53cb2f96ffb6789c2ef8d
MIchael-Truong111 <mtruong@binghamton.edu>
2024-11-23 17:24:12 -0500

    update

--- template_final_project-master/main.py
+++ template_final_project-master/main.py
@@ -2,28 +2,45 @@ import pygame, sys
 #import your controller
 pygame.init()
 screen_width = 800
-screen_height = 400
+screen_height = 800
 screen = pygame.display.set_mode((screen_height, screen_width))
-screen.fill('black')
 
-def main():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        pygame.display.update()
-    #Create an instance on your controller object
-    #Call your mainloop
+#create sprites https://coderslegacy.com/python/pygame-platformer-game-development/
+
 
 # make rectangle
+speed = pygame.math.Vector2
+ACC = 1
 class Rectangle(pygame.sprite.Sprite):
-    def __init__(self, width, length):
-        self.w = width
-        self.l = length
-rectangle_1 = Rectangle(200,200)
-rectangle_2 = Rectangle(200,200)
+    def __init__(self, locx, locy):
+        super().__init__()
+        self.surf = pygame.Surface((135, 10))
+        self.surf.fill('white')
+        self.image = self.surf
+        self.rect = self.surf.get_rect(center = (locx, locy))
+        self.acc = speed(0, 0)
+    def movement(self, l, r):
+        key = pygame.key.get_pressed()
+        if key[l]:
+            self.acc.x = -ACC
+        if key[r]:
+            self.acc.x = ACC
+        
+        self.rect.x += self.acc.x  
+
+
+
+
+
+
+rectangle_1 = Rectangle(385, 10)
+rectangle_2 = Rectangle(386, 790)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(rectangle_1, rectangle_2)
+
 """
+
+
 # make ball
 class PBall(pygame.sprite.Sprite):
     def __init__(self,):
@@ -38,6 +55,20 @@ while True:
                 print('rigt')
 
 """
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        rectangle_1.movement(pygame.K_LEFT, pygame.K_RIGHT)
+        rectangle_2.movement(pygame.K_a, pygame.K_d)
+        screen.fill('black')
+        all_sprites.draw(screen)
+        pygame.display.flip()
+    #Create an instance on your controller object
+    #Call your mainloop
+
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 # https://www.geeksforgeeks.org/how-to-get-keyboard-input-in-pygame/
 # https://coderslegacy.com/python/python-pygame-tutorial/
