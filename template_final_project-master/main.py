799053a2e80a1c6794bb6c039133ecf91273f152
MIchael-Truong111 <mtruong@binghamton.edu>
2024-11-24 17:06:09 -0500

    update

--- template_final_project-master/main.py
+++ template_final_project-master/main.py
@@ -1,10 +1,11 @@
-import pygame, sys
+import pygame, sys, random
 #import your controller
 pygame.init()
 screen_width = 800
 screen_height = 800
 screen = pygame.display.set_mode((screen_height, screen_width))
-
+tscore = 0
+bscore = 0
 #create sprites https://coderslegacy.com/python/pygame-platformer-game-development/
 
 
@@ -33,34 +34,34 @@ class Rectangle(pygame.sprite.Sprite):
             self.acc.x = 0
 
         self.rect.x += self.acc.x  
-
-
-
-
-
-
 rectangle_1 = Rectangle(385, 10)
 rectangle_2 = Rectangle(386, 790)
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((12, 12)) # create abackground for ball
+        self.image = self.surf 
+        self.surf.fill('black') # fill the background for the ball
+        self.ball = pygame.draw.circle(self.image, 'white', (6, 6), 6)
+        self.rect = self.surf.get_rect(center = (400, 400))
+
+pong = Ball()
+
+
+class Dividor(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((800, 2))
+        self.image = self.surf
+        self.surf.fill('white')
+        self.line = pygame.draw.line(self.image, 'white', (0, 400), (800, 400))
+        self.rect = self.surf.get_rect(center = (400, 400))
+
+half = Dividor()
 all_sprites = pygame.sprite.Group()
-all_sprites.add(rectangle_1, rectangle_2)
+all_sprites.add(rectangle_1, rectangle_2, pong, half)
 
-"""
-
-
-# make ball
-class PBall(pygame.sprite.Sprite):
-    def __init__(self,):
- 
-# make keyboard inputs
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                print("a")
-            if event.key == pygame.K_d:
-                print('rigt')
-
-"""
 def main():
     while True:
         for event in pygame.event.get():
@@ -69,6 +70,7 @@ def main():
                 sys.exit()
         rectangle_1.movement(pygame.K_LEFT, pygame.K_RIGHT)
         rectangle_2.movement(pygame.K_a, pygame.K_d)
+        
         screen.fill('black')
         all_sprites.draw(screen)
         pygame.display.flip()
